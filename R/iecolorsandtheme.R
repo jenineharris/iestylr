3e0f0a36b35b092bbce3e2ee0e38ca9380b87e3c
jenineharris <harrisj@wustl.edu>
2026-01-20 16:58:31 -0600

    update functions

--- R/iecolorsandtheme.R
+++ R/iecolorsandtheme.R
@@ -1,44 +1,133 @@
 ################### IE COLOR PALETTES
 
-palette_categorical <- c(
-  "Teal"      = "#1B9CAB",
-  "Burgundy"  = "#993A53",
-  "Yellow"    = "#F4C151",
-  "Dark Blue" = "#2A6092",
-  "Orange"    = "#D68643",
-  "Sky Blue"  = "#64AEE6",
-  "Tan"       = "#D1A686",
-  "Mint"      = "#9ED3DB",
-  "Blue-Gray" = "#8CA6BC"
+# R/iecolorsandtheme.R
+
+ie_palettes <- list(
+  palette_categorical = c(
+    "Teal"      = "#1B9CAB",
+    "Burgundy"  = "#993A53",
+    "Yellow"    = "#F4C151",
+    "Dark Blue" = "#2A6092",
+    "Orange"    = "#D68643",
+    "Sky Blue"  = "#64AEE6",
+    "Tan"       = "#D1A686",
+    "Mint"      = "#9ED3DB",
+    "Blue-Gray" = "#8CA6BC"
+  )
+
+  palette_sequential = c(
+    "Teal"      = "#1B9CAB",
+    "Teal2"      = "#43A6B4",
+    "Teal3"      = "#5EB0BC",
+    "Teal4"      = "#75BBC5",
+    "Teal5"      = "#8BC5CE",
+    "Teal6"      = "#9FD0D7",
+    "Teal7"      = "#B4DAE0",
+    "Teal8"      = "#C7E4E9",
+    "Teal9"      = "#DBEFF2"
+  )
+
+  palette_diverging = c(
+    "Orange"    = "#D68643",
+    "Orange2"    = "#DB975E",
+    "Orange3"    = "#E6B48E",
+    "Orange4"    = "#EED2BE",
+    "Cream" = "#FDF0D5",
+    "Teal4"      = "#C5DBDF",
+    "Teal3"      = "#98C6CD",
+    "Teal2"      = "#66B1BC",
+    "Teal"      = "#1B9CAB"
+  )
 )
 
-palette_sequential <- c(
-  "#1B9CAB",
-  "#43A6B4",
-  "#5EB0BC",
-  "#75BBC5",
-  "#8BC5CE",
-  "#9FD0D7",
-  "#B4DAE0",
-  "#C7E4E9",
-  "#DBEFF2"
-)
 
-palette_diverging <- c(
-  "#D68643",
-  "#DB975E",
-  "#E6B48E",
-  "#EED2BE",
-  "#FDF0D5",
-  "#C5DBDF",
-  "#98C6CD",
-  "#66B1BC",
-  "#1B9CAB"
-)
+# Function to generate color palettes (discrete/continuous)
+# Helper function to generate palettes
+my_pal_gen <- function(palette = "palette_categorical", direction = 1, ...) {
+  pal <- my_palettes[[palette]]
+  if (direction >= 0) {
+    pal <- pal
+  } else {
+    pal <- rev(pal)
+  }
 
-################### IE GGPLOT DEFAULT THEME
+  grDevices::colorRampPalette(pal, ...)
+}
 
-theme_default <- function() {
+#' Custom ggplot2 color scales
+#'
+#' @param palette Name of the color palette (e.g., "palette_categorical").
+#' @param direction Direction of the color scale (1 for default, -1 for reversed).
+#' @param discrete Logical, use a discrete color scale?
+#' @param ... Additional arguments passed to ggplot2::scale_color_gradientn or ggplot2::scale_color_manual.
+#' @export
+scale_color_my_pal <- function(palette = "palette_categorical", direction = 1, discrete = TRUE, ...) {
+  if (discrete) {
+    ggplot2::scale_color_manual(values = my_palettes[[palette]], ...)
+  } else {
+    pal_function <- my_pal_gen(palette, direction)
+    ggplot2::scale_color_gradientn(colors = pal_function(256), ...)
+  }
+}
+
+#' Custom ggplot2 fill scales
+#'
+#' @param palette Name of the color palette (e.g., "palette_categorical").
+#' @param direction Direction of the color scale (1 for default, -1 for reversed).
+#' @param discrete Logical, use a discrete fill scale?
+#' @param ... Additional arguments passed to ggplot2::scale_fill_gradientn or ggplot2::scale_fill_manual.
+#' @export
+scale_fill_my_pal <- function(palette = "palette_categorical", direction = 1, discrete = TRUE, ...) {
+  if (discrete) {
+    ggplot2::scale_fill_manual(values = my_palettes[[palette]], ...)
+  } else {
+    pal_function <- my_pal_gen(palette, direction)
+    ggplot2::scale_fill_gradientn(colors = pal_function(256), ...)
+  }
+}
+
+
+# Document and export the function with roxygen comments
+#' My Package Color Scales
+#'
+#' @description A set of color scales for ggplot2 using the mypackage palettes.
+#' @param palette Name of the color palette.
+#' @param reverse Logical, reverse the direction of the palette.
+#' @param ... Additional arguments passed to discrete_scale or continuous_scale.
+#' @export
+#' @examples
+#' library(ggplot2)
+#' ggplot(iris, aes(x = Species, fill = Species)) +
+#'   geom_bar() +
+#'   scale_fill_mypackage_d()
+scale_fill_mypackage_d <- function(palette = "main", reverse = FALSE, ...) {
+  ggplot2::discrete_scale("fill", "mypackage_d", mypackage_palettes(palette, reverse = reverse), ...)
+}
+
+#' @export
+scale_color_mypackage_d <- function(palette = "main", reverse = FALSE, ...) {
+  ggplot2::discrete_scale("color", "mypackage_d", mypackage_palettes(palette, reverse = reverse), ...)
+}
+
+
+
+
+#' IE GGPLOT DEFAULT THEME
+#'
+#' `theme_iedefault()` applies a default theme to match the student outcomes style guide. Further
+#' changes can be made with subsequent calls to [ggplot2::theme()]. `theme_patch()` should be applied to
+#' a "patch" that will be combined with other plots via patchwork. It specifies a smaller title than
+#' `theme_iedefault()` (in this case the title will be the title of a panel rather than the whole plot) and
+#' does not specify formatting for subtitles and captions. `theme_patchwork_annotate()` can be
+#' passed to the `theme` argument of [patchwork::plot_annotation()] to ensure that the text of the
+#' combined plot is properly styled consistent with `theme_default()`
+#'
+#' @return A `ggplot` theme object
+#' @seealso [patchwork_annotate()] for a wrapper that automatically runs [patchwork::plot_annotation()]
+#' and applies `theme_patchwork_annotate()`
+#'
+#' @export
+theme_iedefault <- function() {
   ggplot2::theme_minimal() +
     ggplot2::theme(
       panel.grid = ggplot2::element_blank(),
